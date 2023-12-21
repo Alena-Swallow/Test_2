93c39502de65e4d588e64733eeccbadebad74f7d
Alena-Swallow <154365760+Alena-Swallow@users.noreply.github.com>
2023-12-21 21:07:07 +0300

    Update Test_2.go

--- Test_2.go
+++ Test_2.go
@@ -1,20 +1,18 @@
-go
 package main
 
 import "fmt"
 
 type Pair struct {
-    a, b int
+	a, b int
 }
 
 func (p Pair) calcSum() int {
-    return p.a + p.b
+	return p.a + p.b
 }
 
 func main() {
-    a := 10
-    b := 20
-    pair := Pair{a, b}
-    sum := pair.calcSum()
-    fmt.Printf("Сумма %d и %d равна %d\n", pair.a, pair.b, sum)
-}
+	a := 10
+	b := 20
+	pair := Pair{a, b}
+	sum := pair.calcSum()
+	fmt.Printf("Сумма %d и %d равна %d\n", pair.a, pair.b, sum)
